b7bef50c932b213cecf8f94ea59419ef451e874a
mat <matthew.kelcey@gmail.com>
2009-10-05 23:30:53 +1100

    bug fix

--- all_shortest_paths.rb
+++ all_shortest_paths.rb
@@ -1,21 +1,22 @@
 #!/usr/bin/env ruby
 require 'set'
-
+require 'node'
 
 class AllShortestPaths
 
-	def initialize neighbours
-		@neighbours = neighbours
-		@shortest_path_edges = {}
-		@shortest_path_edges.default = 0
+	def initialize nodes
+		@nodes = nodes
 	end
 		
 	def output_dot
 		dot = File.open("test.dot","w")
 		dot.puts "digraph {"
-		@neighbours.each do |from,neighbour_list|
-			neighbour_list.each do |neighbour|
-				dot.puts "#{from} -> #{neighbour} [penwidth=\"#{@shortest_path_edges[[from,neighbour]]}\"]"
+		@nodes.each do |node|
+			node.neighbours.each do |neighbour|
+				number_times_edge_crossed = @shortest_path_edges[[node, neighbour]]
+				width = number_times_edge_crossed#Math.log(number_times_edge_crossed+1)
+				puts "\"#{node.id}\" -> \"#{neighbour.id}\" [penwidth=\"#{width}\"]"
+				dot.puts "\"#{node.id}\" -> \"#{neighbour.id}\" [penwidth=\"#{width}\"]"
 			end
 		end
 		dot.puts "}"
@@ -23,8 +24,9 @@ class AllShortestPaths
 	end
 
 	def all_shortest_paths
-		@neighbours.keys.each { |k| shortest_path_from k }
-		@shortest_path_edges.values.each { |v| puts v }
+		@shortest_path_edges = {}
+		@shortest_path_edges.default = 0
+		@nodes.each { |k| shortest_path_from k }
 	end
 
 	def shortest_path_from start_node
@@ -33,10 +35,6 @@ class AllShortestPaths
 		queue = []
 		queue << start_node
 
-		#distances = {}
-		#distances.default = 1.0/0
-		#distances[start_node] = 0
-
 		visited = Set.new
 		shortest_found = Set.new
 		shortest_found << start_node
@@ -44,10 +42,10 @@ class AllShortestPaths
 		while !queue.empty?
 			node = queue.shift
 			visited << node
-			next unless @neighbours.has_key? node
-			@neighbours[node].each do |neighbour|
-				if (!shortest_found.include? neighbour)
-					@shortest_path_edges[[node,neighbour]] += 1
+#			next unless node.neighbours.include? node
+			node.neighbours.each do |neighbour|
+				if !(shortest_found.include? neighbour)
+					@shortest_path_edges[[node, neighbour]] += 1
 					shortest_found << neighbour
 				end
 				queue << neighbour unless visited.include?(neighbour) || queue.include?(neighbour)
@@ -58,38 +56,15 @@ class AllShortestPaths
 
 end
 
-=begin
-neighbours = {
-0=>[1,3],
-1=>[2],
-2=>[6,5,4,3],
-3=>[4],
-4=>[],
-5=>[8,9,4,0],
-6=>[1,7,8,5],
-7=>[8,4],
-8=>[9],
-9=>[4,0],
-10 => [11],
-11 => [12],
-12 => [10],
-}
-=end
-
 raise "all_shortest_paths.rb <EDGES>" unless ARGV.length==1
 EDGES_FILE = ARGV[0]
 
-neighbours = {}
-File.open(EDGES_FILE).each do |edge|
-	from,to = edge.strip.split("\t")
-	neighbours[from] ||= []
-	neighbours[from] << to	
-end
+nodes = Node.hash_to_nodes_from_file EDGES_FILE
 #puts neighbours.inspect
 
-asp = AllShortestPaths.new neighbours
+asp = AllShortestPaths.new nodes
 asp.all_shortest_paths
-#asp.output_dot
+asp.output_dot
 
 # barplot(sort(table(scan("passes"))))
 
